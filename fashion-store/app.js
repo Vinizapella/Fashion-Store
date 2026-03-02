1ba5130add839e1283b9ff32bbed288db0d2faea
Vinícius dos Santos Zapella <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-02 11:48:42 -0300

    feat: estudo para a prova, sem o DOM

--- fashion-store/app.js
+++ fashion-store/app.js
@@ -1,64 +1,76 @@
-const BASE_URL = "https://api.escuelajs.co/api/v1";
+const btn = document.getElementById('theme-toggle');
+/* Pegamos o botao que recebe o dados */
+const html = document.documentElement;
+/* Pegamos uma constante do html ele faz esse document.documentElement dizendo que todos os elementos do corpo html irao mudar */ 
 
-document.addEventListener('DOMContentLoaded', () =>{
-
-    const eIndex = document.querySelector('#featured-list');
-
-    if(eIndex){
-        getFeaturedProducts();
-    }
+btn.addEventListener('click', () =>{
+    /* Com o evento addEventListener escutamos o click que vai gerar uma ação */
+    const currentTheme = html.getAttribute('data-theme');
+    /* fazemos uma constante do tema atual, faz o html fazer um get verificando o tema que esta atualmente presente */
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light'
+    /* Um novo tema que vai ser igual meu tema atual que vai fazer uma verificação de qual tema esta */
+    html.setAttribute('data-theme', newTheme);
+    /* setamos o html com o novo tema */
+    localStorage.setItem('data-theme', newTheme);
+    /* deixamos guardados no localStorage o ultimo tema */ 
 });
 
-async function getFeaturedProducts() {
- 
-    const featuredList = document.getElementById('featured-list');
+const api = 'https://api.escuelajs.co/api/v1';
 
+async function buscaDados() {
+    /* Criamos a função de buscar o dadaos na Api */
     try{
-        const response = await fetch(`${BASE_URL}/products`);
+        const result = await fetch(`${api}/products`);
+        /* salvamos o dados retirados no fetch em uma variavel */
 
-        if(!response.ok)throw new Error('Erro ao pegar os dados da API');
+        if(!result.ok) throw new Error("Erro ao verificar os dados");
+        /* pequena verificação */
 
-        const products = await response.json();
+        const dadosApi = await result.json();
+        /* com os dados salvos pegamos e salvamos em um objeto json */
 
-        const onlyThree = products.slice(0, 3);
+        const dadosFormatados = dadosApi.slice(0,3);
+        /* formatamos para receber os destaques */
 
-        featuredList.innerHTML = "";
-
-        onlyThree.forEach(product => {
-            const card = `
-                <article class="card">
-                    <div class="card-img-wrapper">
-                        <img src="${product.images[0]}" alt="${product.title}" class="card-img">
-                    </div>
-                    <div class="card-content">
-                        <span class="card-category">${product.category.name}</span>
-                        <h3 class="card-title">${product.title}</h3>
-                        <div class="card-footer">
-                            <span class="card-price">${product.price}</span>
-                            <a href="detail.html?id=${product.id}" class=btn-primary btn-small">Ver detalhes</a>
-                        </div>
-                    </div>
-                </article>
-            `;
-            featuredList.innerHTML += card;
-        });
-    }catch(error){
-        console.error('Erro:', error);
-        featuredList.innerHTML = '<p> class="error"> Não foi possível se conectar ao Banco de Dados.</p> ';
+        exibirDados(dadosFormatados);
+        /* chamamos nossa função de exibir retornando como parametro os dados formatados */
+    }catch (error){
+        throw new Error("Erro na api")
     }
+}
+
+function exibirDados(dadosFormatados){
+    /* criamos a função cmo os dados formatados de parametro */
+
+    const container = document.getElementById('featured-list');
+    /* pegamos pelo id o lugar aonde vamos formatar */
+
+    container.innerHTML = " ";
+    /* limpamos o espaço */
+    
+    dadosFormatados.forEach(produto => {
+    /* faz ele percorrer pela lista */    
+        const card = `
+        <article class="placeholder-card">
+            <div class="card-img-wrapper">
+                <img src="${produto.images[0]}" alt="${produto.title}" class="card-img">
+            </div>
+            <div class="card-content">
+                <span class="card-category">${produto.category.name}</span>
+                <h3 class="card-title">${produto.title}></h3>
+                <div class="card-footer">
+                    <span class="card-price">${produto.price}</span>
+                    <a href="#">Ver mais</a>
+                </div>
+            </div>
+        </article>
+        `;
+        /* maior resumo, ele e a msm coisa do html, porem aonde tem coisa escrita no html colocamos a mudança com o produto. algo */
+
+        container.innerHTML += card;
+    });
 
 }
 
-
-
-
-
-
-function toggleTheme(){
-        const html = document.documentElement;
-        const currentTheme = html.getAttribute('data-theme');
-        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-
-        html.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-}
+buscaDados();
+/* muitooo importante */
